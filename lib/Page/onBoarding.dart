a09ca2f4e1bf4ef7fc2d33d9691ef61687fe2437
erde123 <108157433+erde123@users.noreply.github.com>
2024-03-22 10:18:20 +0700

    update OnBoarding

--- lib/Page/onBoarding.dart
+++ lib/Page/onBoarding.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_coffee_application/Page/admin/home_admin.dart';
 import 'package:flutter_coffee_application/Page/admin/scanqr.dart';
+import 'package:flutter_coffee_application/Page/auth/auth_page.dart';
 import 'package:flutter_coffee_application/Page/auth/login.dart';
 import 'package:flutter_coffee_application/Page/auth/signup.dart';
 import 'package:flutter_coffee_application/Page/home.dart';
@@ -11,6 +13,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../component/TextField.dart';
 import '../style/typhography.dart';
 
 class OnBoarding extends ConsumerStatefulWidget {
@@ -26,122 +29,208 @@ class _OnBoardingState extends ConsumerState<OnBoarding> {
     'assets/image/ver2.jpeg',
     'assets/image/ver3.jpeg',
   ];
+  final email = TextEditingController();
+  final pass = TextEditingController();
+  final emailNode = FocusNode();
+  final passNode = FocusNode();
+  bool validate = false;
+  bool enable = false;
+
+  @override
+  void initState() {
+    email.addListener(validateInputs);
+    pass.addListener(validateInputs);
+    emailNode.addListener(_onFocusChange);
+    passNode.addListener(_onFocusChange);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    email.dispose();
+    pass.dispose();
+    emailNode.removeListener(_onFocusChange);
+    passNode.removeListener(_onFocusChange);
+    super.dispose();
+  }
+
+  void validateInputs() {
+    if (email.text.isEmpty || pass.text.isEmpty) {
+      setState(() {
+        enable = false;
+      });
+    } else {
+      setState(() {
+        enable = true;
+      });
+    }
+  }
+
+  void _onFocusChange() {
+    debugPrint("Email Focus: ${emailNode.hasFocus.toString()}");
+    debugPrint("Pass Focus: ${passNode.hasFocus.toString()}");
+  }
+
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
-      child: Scaffold(
-        body: Container(
-          color: primary,
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Stack(
-            children: [
-              Builder(
-                builder: (context) {
-                  final double height = MediaQuery.of(context).size.height;
-                  return CarouselSlider(
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      height: height,
-                      viewportFraction: 1.0,
-                      enlargeCenterPage: false,
-                    ),
-                    items: imageList
-                        .map((item) => Container(
-                              child: Center(
-                                  child: Image.asset(
-                                item,
-                                fit: BoxFit.cover,
-                                height: height,
-                              )),
-                            ))
-                        .toList(),
-                  );
-                },
-              ),
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 2,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      stops: const [
-                        0.1,
-                        0.2,
-                        0.3,
-                        0.6,
-                      ],
-                      colors: [
-                        Colors.white.withOpacity(0),
-                        Colors.white.withOpacity(0.3),
-                        Colors.white.withOpacity(0.7),
-                        Colors.white,
-                      ],
+      child: SafeArea(
+        child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: SingleChildScrollView(
+            child: Container(
+              color: primary,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  Builder(
+                    builder: (context) {
+                      final double height = MediaQuery.of(context).size.height;
+                      return Positioned.fill(
+                        child: CarouselSlider(
+                          options: CarouselOptions(
+                            autoPlay: true,
+                            height: height,
+                            viewportFraction: 1.0,
+                            enlargeCenterPage: false,
+                          ),
+                          items: imageList
+                              .map((item) => Container(
+                                    child: Center(
+                                        child: Image.asset(
+                                      item,
+                                      fit: BoxFit.cover,
+                                      height: height,
+                                    )),
+                                  ))
+                              .toList(),
+                        ),
+                      );
+                    },
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height / 1.6,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: const [
+                            0.1,
+                            0.2,
+                            0.3,
+                            0.6,
+                          ],
+                          colors: [
+                            Colors.white.withOpacity(0),
+                            Colors.white.withOpacity(0.3),
+                            Colors.white.withOpacity(0.7),
+                            Colors.white,
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Positioned(
-                bottom: 50,
-                left: 0,
-                right: 0,
-                child: Column(
-                  children: [
-                    ButtonOutline(
-                      onPressed: () {
-                        AuthServices().signInWithGoogle();
-                      },
-                      width: MediaQuery.of(context).size.width - 32,
-                      height: 42,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  Positioned(
+                    bottom: 30,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const CircleAvatar(
-                            backgroundColor: Colors.transparent,
-                            backgroundImage:
-                                AssetImage("assets/image/google.png"),
-                            radius: 12,
+                          Text(
+                            "Masukkan email",
+                            style: h2(color: primary),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          LoginTextField(
+                            focus: emailNode,
+                            controller: email,
+                            textInputType: TextInputType.emailAddress,
+                            error: validate ? "belum diisi" : null,
+                            hint: "email",
+                            type: "email",
+                            onNext: () => FocusScope.of(context).requestFocus(
+                                passNode), // Focus on password TextField
+                          ),
+                          const SizedBox(
+                            height: 10,
                           ),
                           Text(
-                            "Lanjutkan dengan Google",
-                            style: h3(
-                              color: primary,
+                            "Masukkan password",
+                            style: h2(color: primary),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          LoginTextField(
+                            focus: passNode,
+                            controller: pass,
+                            textInputType: TextInputType.text,
+                            error: validate ? "belum diisi" : null,
+                            hint: "password",
+                            type: "pass",
+                          ),
+                          const SizedBox(
+                            height: 65,
+                          ),
+                          ButtonSubmit(
+                            enable: enable,
+                            onPressed: () async {
+                              setState(() {
+                                validate = email.text.isEmpty;
+                                validate = pass.text.isEmpty;
+                              });
+
+                              if (!validate) {
+                                try {
+                                  final UserCredential userCredential =
+                                      await FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                    email: email.text,
+                                    password: pass.text,
+                                  );
+                                  // Authentication successful, navigate to appropriate page
+                                  if (userCredential.user != null) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              HomeAdmin()), // Navigate to Home page after successful authentication
+                                    );
+                                  }
+                                } catch (e) {
+                                  // Handle authentication errors here
+                                  print("Error signing in: $e");
+                                  // You can show an error message to the user using a snackbar or dialog
+                                }
+                              }
+                            },
+                            width: MediaQuery.of(context).size.width - 40,
+                            height: 55,
+                            child: Text(
+                              "Lanjutkan",
+                              style: h3(color: Colors.white),
                             ),
                           ),
                         ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(foregroundColor: Colors.white),
-                      child: Text(
-                        "Lewati tahap Ini",
-                        style: h3(
-                          color: primary,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          PageTransition(
-                              child: Home(),
-                              type: PageTransitionType.rightToLeft),
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/component/TextField.dart
+++ lib/component/TextField.dart
@@ -8,39 +8,68 @@ class LoginTextField extends ConsumerStatefulWidget {
   final String? error;
   final String hint;
   final FocusNode focus;
+  final String? type;
+  final VoidCallback? onNext; // New callback to handle "Next" button
 
   const LoginTextField({
-    super.key,
+    Key? key,
     required this.controller,
     required this.textInputType,
     required this.error,
     required this.hint,
     required this.focus,
-  });
+    required this.type,
+    this.onNext, // Initialize the new callback
+  }) : super(key: key);
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _LoginTextFieldState();
 }
 
 class _LoginTextFieldState extends ConsumerState<LoginTextField> {
+  bool obscureText = true; // Variable to control password visibility
+
+  void togglePasswordVisibility() {
+    setState(() {
+      obscureText = !obscureText; // Toggle visibility
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextField(
       focusNode: widget.focus,
       controller: widget.controller,
       keyboardType: widget.textInputType,
+      textInputAction: widget.onNext != null
+          ? TextInputAction.next
+          : null, // Set TextInputAction based on onNext callback
+      onSubmitted: (_) {
+        if (widget.onNext != null) {
+          widget.onNext!(); // Call onNext callback
+        }
+      },
+      obscureText: widget.type == "pass" ? obscureText : false, // Show/hide password text
       maxLines: 1,
       decoration: InputDecoration(
         errorText: widget.error,
         hintText: widget.hint,
         suffixIcon: widget.focus.hasFocus
-            ? IconButton(
-              onPressed: () => widget.controller.clear(),
-              icon: Icon(
-                  Icons.close,
-                  color: primary,
-                ),
-            )
+            ? widget.type == "email"
+                ? IconButton(
+                    onPressed: () => widget.controller.clear(),
+                    icon: Icon(
+                      Icons.close,
+                      color: primary,
+                    ),
+                  )
+                : IconButton(
+                    onPressed: togglePasswordVisibility, // Toggle password visibility
+                    icon: Icon(
+                      obscureText ? Icons.visibility_off : Icons.visibility, // Change icon based on visibility state
+                      color: primary,
+                    ),
+                  )
             : null,
       ),
     );
